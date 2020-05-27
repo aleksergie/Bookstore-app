dae6abf88761e9ae4a3608e9381318e87e07787b
aleksergie <aleksergie@ukr.net>
2020-05-27 14:38:41 +0300

    Added lodash for handling sorting

--- src/containers/App.container.js
+++ src/containers/App.container.js
@@ -2,9 +2,25 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as bookActions from "../redux/books/books.actions";
 import App from "../components/App.component";
+import orderBy from "lodash/orderBy";
+
+const sortBy = (books, filteredBy) => {
+  switch (filteredBy) {
+    case "all":
+      return books;
+    case "priceHigh":
+      return orderBy(books, "price", "desc");
+    case "priceLow":
+      return orderBy(books, "price", "asc");
+    case "author":
+      return orderBy(books, "author", "asc");
+    default:
+      return books;
+  }
+};
 
 const mapStateToProps = ({ books }) => ({
-  books: books.items,
+  books: sortBy(books.items, books.filteredBy),
   isReady: books.isReady,
 });
 
